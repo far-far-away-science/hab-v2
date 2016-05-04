a730686fee2531d8eab1319ff243c1270d2e0eb4
Pavel Krupets <usaguerrilla@hotmail.com>
2016-05-03 22:04:10 -0700

    added nmeaReceiveCharacter tests and fixes

--- software/com-telemetry/src/gps/nmeaBuffer.c
+++ software/com-telemetry/src/gps/nmeaBuffer.c
@@ -35,46 +35,53 @@ void nmeaReceiveCharacter(NmeaRingBuffer* pNmeaRingBuffer, uint8_t character, bo
         pNmeaRingBuffer->statusMask &= ~NRBS_FULL;
     }
 
-    if (!(pNmeaRingBuffer->statusMask & NRBS_FULL) && !(pNmeaRingBuffer->statusMask & NRBS_WAIT_UNTIL_NEXT_MESSAGE)) {
-        const uint8_t charWriteIdx = pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size;
-        const bool thereIsSpaceForNewCharacter = charWriteIdx < NMEA_MESSAGE_MAX_LEN;
-
-        bool moveToNewMessage = false;
-        bool placeCurrentCharToNewMessage = false;
-
-        if (character == '$' && charWriteIdx != 0) {
-            moveToNewMessage = true;
-            placeCurrentCharToNewMessage = true;
-            pNmeaRingBuffer->statusMask &= ~NRBS_WAIT_UNTIL_NEXT_MESSAGE; // we already at the beginning of next message
-        } else if (!thereIsSpaceForNewCharacter) {
-            moveToNewMessage = true;
-            pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].hasError = true;
-            pNmeaRingBuffer->statusMask |= NRBS_WAIT_UNTIL_NEXT_MESSAGE; // message buffer overflow, skip until next message
-        } else {
-            pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].message[charWriteIdx] = character;
-            ++pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size;
+    if (!(pNmeaRingBuffer->statusMask & NRBS_FULL)) {
+        if (character == '$') {
+            pNmeaRingBuffer->statusMask &= ~NRBS_WAIT_UNTIL_NEXT_MESSAGE; // we are at the beginning of next message
         }
 
-        if (moveToNewMessage) {
-            // start index can only get away and cannot get past end index so we are fine here
-            pNmeaRingBuffer->endIdx = advanceUint8Index(pNmeaRingBuffer->endIdx, NMEA_RING_BUFFER_MAX_MESSAGES_COUNT);
-            // if start index moves on we are golden and can continue writing
-            if (pNmeaRingBuffer->endIdx == pNmeaRingBuffer->startIdx) {
-                pNmeaRingBuffer->statusMask |= NRBS_FULL;
+        if (!(pNmeaRingBuffer->statusMask & NRBS_WAIT_UNTIL_NEXT_MESSAGE)) {
+            const uint8_t charWriteIdx = pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size;
+            const bool thereIsSpaceForNewCharacter = charWriteIdx < NMEA_MESSAGE_MAX_LEN;
+
+            bool moveToNewMessage = false;
+            bool placeCurrentCharToNewMessage = false;
+
+            if (!thereIsSpaceForNewCharacter) {
+                moveToNewMessage = true;
+                pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].hasError = true;
+                pNmeaRingBuffer->statusMask |= NRBS_WAIT_UNTIL_NEXT_MESSAGE; // message buffer overflow, skip until next message
+            } else if (character != '$' && charWriteIdx == 0) {
+                pNmeaRingBuffer->statusMask |= NRBS_WAIT_UNTIL_NEXT_MESSAGE; // unexpected start character
+            } else if (character == '$' && charWriteIdx != 0) {
+                moveToNewMessage = true;
+                placeCurrentCharToNewMessage = true;
             } else {
-                if (placeCurrentCharToNewMessage) {
-                    pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].message[0] = character;
-                    pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size = 1;
+                pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].hasError |= errorEncountered;
+                pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].message[charWriteIdx] = character;
+                ++pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size;
+                moveToNewMessage = character == '\x0A'; // we are done with the current message if we met LF
+            }
+
+            if (moveToNewMessage) {
+                // start index can only get away and cannot get past end index so we are fine here
+                pNmeaRingBuffer->endIdx = advanceUint8Index(pNmeaRingBuffer->endIdx, NMEA_RING_BUFFER_MAX_MESSAGES_COUNT);
+                // if start index moves on we are golden and can continue writing
+                if (pNmeaRingBuffer->endIdx == pNmeaRingBuffer->startIdx) {
+                    pNmeaRingBuffer->statusMask |= NRBS_FULL;
                 } else {
-                    pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size = 0;
+                    if (placeCurrentCharToNewMessage) {
+                        pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].hasError = errorEncountered;
+                        pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].message[0] = character;
+                        pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size = 1;
+                    } else {
+                        pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].hasError = false;
+                        pNmeaRingBuffer->buffer[pNmeaRingBuffer->endIdx].size = 0;
+                    }
                 }
             }
         }
     } else {
         ERROR_NMEA_BUFFER_OVERFLOW();
-    }
-
-    if ((pNmeaRingBuffer->statusMask & NRBS_WAIT_UNTIL_NEXT_MESSAGE) && character == '\x0A') {
-        pNmeaRingBuffer->statusMask &= ~NRBS_WAIT_UNTIL_NEXT_MESSAGE;
     }
 }
