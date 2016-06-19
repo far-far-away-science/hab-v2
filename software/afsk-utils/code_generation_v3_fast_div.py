3d447ec1dd6444d5b1affcd8e1e1052c88d4c10a
Pavel Krupets <usaguerrilla@hotmail.com>
2016-06-19 13:13:36 -0700

    more bug fixes (overflows as type used was uint16_t not uint32_t)

--- software/afsk-utils/code_generation_v3_fast_div.py
+++ software/afsk-utils/code_generation_v3_fast_div.py
@@ -16,9 +16,9 @@ class Generator:
     def formatBestFastDivision(self, fastDivisionAlias):
         (firstDivisorPowerOfTwo, multiplier, lastDivisorPowerOfTwo, precisionSummand, clampValue) = self.modulation.getBestFastDivision(fastDivisionAlias)
         if firstDivisorPowerOfTwo != 0:
-            return 'CLAMP((((((value) + ' + str(precisionSummand) + ') >> ' + str(firstDivisorPowerOfTwo) + ') * ' + str(multiplier) + ') >> ' + str(lastDivisorPowerOfTwo) + '), ' + str(clampValue) + ')'
+            return 'CLAMP(((((((uint32_t) value) + ' + str(precisionSummand) + ') >> ' + str(firstDivisorPowerOfTwo) + ') * ' + str(multiplier) + ') >> ' + str(lastDivisorPowerOfTwo) + '), ' + str(clampValue) + ')'
         else:
-            return 'CLAMP(((((value) + ' + str(precisionSummand) + ') * ' + str(multiplier) + ') >> ' + str(lastDivisorPowerOfTwo) + ')' + str(clampValue) + ')'
+            return 'CLAMP((((((uint32_t) value) + ' + str(precisionSummand) + ') * ' + str(multiplier) + ') >> ' + str(lastDivisorPowerOfTwo) + ')' + str(clampValue) + ')'
 
     def generateDefinitionsHeader(self, filePath):
         text = '''#pragma once
@@ -46,37 +46,37 @@ class Generator:
 // code_generation_v2.py file
 //
 
-#define APRS_SIGNAL_GENERATION_FREQUENCY ''' + str(definitions.APRS_SIGNAL_GENERATION_FREQUENCY) + '''
+#define APRS_SIGNAL_GENERATION_FREQUENCY ((uint32_t)''' + str(definitions.APRS_SIGNAL_GENERATION_FREQUENCY) + ''')
 
-#define QUANT_MIN_VALUE ''' + str(definitions.QUANT_MIN_VALUE) + '''
-#define QUANT_STEP_SIZE ''' + str(self.modulation.CONST_QUANT_STEP_SIZE.getInternalRepresentation()) + '''
+#define QUANT_MIN_VALUE ((uint32_t)''' + str(definitions.QUANT_MIN_VALUE) + ''')
+#define QUANT_STEP_SIZE ((uint32_t)''' + str(self.modulation.CONST_QUANT_STEP_SIZE.getInternalRepresentation()) + ''')
 
-#define SCALED_PI_OVER_TWO    ''' + str(self.modulation.CONST_PI_OVER_TWO.getInternalRepresentation()) + '''
-#define SCALED_THREE_HALFS_PI ''' + str(self.modulation.CONST_THREE_HALFS_PI.getInternalRepresentation()) + '''
+#define SCALED_PI_OVER_TWO    ((uint32_t)''' + str(self.modulation.CONST_PI_OVER_TWO.getInternalRepresentation()) + ''')
+#define SCALED_THREE_HALFS_PI ((uint32_t)''' + str(self.modulation.CONST_THREE_HALFS_PI.getInternalRepresentation()) + ''')
 
-#define HALF_PERIOD_F1200 ''' + str(self.modulation.CONST_HALF_PERIOD_F1200.getInternalRepresentation()) + '''
-#define HALF_PERIOD_F2200 ''' + str(self.modulation.CONST_HALF_PERIOD_F2200.getInternalRepresentation()) + '''
-                         
-#define QUANTS_COUNT_PER_SYMBOL_F1200 ''' + str(self.modulation.CONST_F1200_QUANTS_COUNT_PER_SYMBOL.getInternalRepresentation()) + '''
-#define QUANTS_COUNT_PER_SYMBOL_F2200 ''' + str(self.modulation.CONST_F2200_QUANTS_COUNT_PER_SYMBOL.getInternalRepresentation()) + '''
+#define HALF_PERIOD_F1200 ((uint32_t)''' + str(self.modulation.CONST_HALF_PERIOD_F1200.getInternalRepresentation()) + ''')
+#define HALF_PERIOD_F2200 ((uint32_t)''' + str(self.modulation.CONST_HALF_PERIOD_F2200.getInternalRepresentation()) + ''')
+
+#define QUANTS_COUNT_PER_SYMBOL_F1200 ((uint32_t)''' + str(self.modulation.CONST_F1200_QUANTS_COUNT_PER_SYMBOL.getInternalRepresentation()) + ''')
+#define QUANTS_COUNT_PER_SYMBOL_F2200 ((uint32_t)''' + str(self.modulation.CONST_F2200_QUANTS_COUNT_PER_SYMBOL.getInternalRepresentation()) + ''')
 
 #define PRECISION_CONVERTER_QUANT_IDX_F1200(value) ''' + self.formatBestFastDivision('CONST_PRECISION_QUANT_DIVISOR_F1200') + '''
 #define PRECISION_CONVERTER_QUANT_IDX_F2200(value) ''' + self.formatBestFastDivision('CONST_PRECISION_QUANT_DIVISOR_F2200') + '''
 
-#define TRIG_PARAM_SCALER_F1200 ''' + str(self.modulation.CONST_TRIG_PARAM_SCALER_F1200.getInternalRepresentation()) + '''
-#define TRIG_PARAM_SCALER_F2200 ''' + str(self.modulation.CONST_TRIG_PARAM_SCALER_F2200.getInternalRepresentation()) + '''
+#define TRIG_PARAM_SCALER_F1200 ((uint32_t)''' + str(self.modulation.CONST_TRIG_PARAM_SCALER_F1200.getInternalRepresentation()) + ''')
+#define TRIG_PARAM_SCALER_F2200 ((uint32_t)''' + str(self.modulation.CONST_TRIG_PARAM_SCALER_F2200.getInternalRepresentation()) + ''')
 #define PRECISION_CONVERTER_TRIG_PARAM_F1200(value) ''' + self.formatBestFastDivision('CONST_PRECISION_TRIG_PARAM_DIVISOR_F1200') + '''
 #define PRECISION_CONVERTER_TRIG_PARAM_F2200(value) ''' + self.formatBestFastDivision('CONST_PRECISION_TRIG_PARAM_DIVISOR_F2200') + '''
 
-#define INVERSE_TRIG_PARAM_SCALER ''' + str(self.modulation.CONST_INVERSE_TRIG_SCALER.getInternalRepresentation()) + '''
+#define INVERSE_TRIG_PARAM_SCALER ((uint32_t)''' + str(self.modulation.CONST_INVERSE_TRIG_SCALER.getInternalRepresentation()) + ''')
 #define PRECISION_CONVERTER_INVERSE_TRIG_PARAM(value) ''' + self.formatBestFastDivision('CONST_PRECISION_INVERSE_TRIG_PARAM_DIVISOR') + '''
 
-#define QUANTS_COUNT_PER_SYMBOL ''' + str(int(definitions_derived.F1200_QUANTS_COUNT_PER_SYMBOL)) + '''
+#define QUANTS_COUNT_PER_SYMBOL ((uint32_t)''' + str(int(definitions_derived.F1200_QUANTS_COUNT_PER_SYMBOL)) + ''')
 
-#define RECIPROCAL_ANGULAR_FREQUENCY_F1200 ''' + str(self.modulation.CONST_RECIPROCAL_ANGULAR_FREQUENCY_F1200.getInternalRepresentation()) + '''
-#define RECIPROCAL_ANGULAR_FREQUENCY_F2200 ''' + str(self.modulation.CONST_RECIPROCAL_ANGULAR_FREQUENCY_F2200.getInternalRepresentation()) + '''
+#define RECIPROCAL_ANGULAR_FREQUENCY_F1200 ((uint32_t)''' + str(self.modulation.CONST_RECIPROCAL_ANGULAR_FREQUENCY_F1200.getInternalRepresentation()) + ''')
+#define RECIPROCAL_ANGULAR_FREQUENCY_F2200 ((uint32_t)''' + str(self.modulation.CONST_RECIPROCAL_ANGULAR_FREQUENCY_F2200.getInternalRepresentation()) + ''')
 
-#define LEADING_WARMUP_QUANTS_COUNT ''' + str(definitions_derived.LEADING_WARMUP_QUANTS_COUNT) + '''
+#define LEADING_WARMUP_QUANTS_COUNT ((uint32_t)''' + str(definitions_derived.LEADING_WARMUP_QUANTS_COUNT) + ''')
 
 extern const uint16_t amplitudeFromTable[];
 extern const uint32_t scaledSineValueFromTable[];
--- software/com-telemetry/src/aprs/generated/afsk.h
+++ software/com-telemetry/src/aprs/generated/afsk.h
@@ -23,37 +23,37 @@
 // code_generation_v2.py file
 //
 
-#define APRS_SIGNAL_GENERATION_FREQUENCY 96000
+#define APRS_SIGNAL_GENERATION_FREQUENCY ((uint32_t)96000)
 
-#define QUANT_MIN_VALUE 0
-#define QUANT_STEP_SIZE 10000
+#define QUANT_MIN_VALUE ((uint32_t)0)
+#define QUANT_STEP_SIZE ((uint32_t)10000)
 
-#define SCALED_PI_OVER_TWO    768
-#define SCALED_THREE_HALFS_PI 2304
+#define SCALED_PI_OVER_TWO    ((uint32_t)768)
+#define SCALED_THREE_HALFS_PI ((uint32_t)2304)
 
-#define HALF_PERIOD_F1200 400000
-#define HALF_PERIOD_F2200 218182
-                         
-#define QUANTS_COUNT_PER_SYMBOL_F1200 800000
-#define QUANTS_COUNT_PER_SYMBOL_F2200 436364
+#define HALF_PERIOD_F1200 ((uint32_t)400000)
+#define HALF_PERIOD_F2200 ((uint32_t)218182)
 
-#define PRECISION_CONVERTER_QUANT_IDX_F1200(value) CLAMP((((((value) + 500) >> 3) * 33) >> 12), 800000)
-#define PRECISION_CONVERTER_QUANT_IDX_F2200(value) CLAMP((((((value) + 500) >> 3) * 66) >> 13), 436364)
+#define QUANTS_COUNT_PER_SYMBOL_F1200 ((uint32_t)800000)
+#define QUANTS_COUNT_PER_SYMBOL_F2200 ((uint32_t)436364)
 
-#define TRIG_PARAM_SCALER_F1200 3840
-#define TRIG_PARAM_SCALER_F2200 7040
-#define PRECISION_CONVERTER_TRIG_PARAM_F1200(value) CLAMP((((((value) + 500000) >> 6) * 67) >> 20), 3072)
-#define PRECISION_CONVERTER_TRIG_PARAM_F2200(value) CLAMP((((((value) + 500000) >> 6) * 67) >> 20), 3072)
+#define PRECISION_CONVERTER_QUANT_IDX_F1200(value) CLAMP(((((((uint32_t) value) + 500) >> 3) * 33) >> 12), 800000)
+#define PRECISION_CONVERTER_QUANT_IDX_F2200(value) CLAMP(((((((uint32_t) value) + 500) >> 3) * 66) >> 13), 436364)
 
-#define INVERSE_TRIG_PARAM_SCALER 750
-#define PRECISION_CONVERTER_INVERSE_TRIG_PARAM(value) CLAMP((((((value) + 500000) >> 6) * 67) >> 20), 3072)
+#define TRIG_PARAM_SCALER_F1200 ((uint32_t)3840)
+#define TRIG_PARAM_SCALER_F2200 ((uint32_t)7040)
+#define PRECISION_CONVERTER_TRIG_PARAM_F1200(value) CLAMP(((((((uint32_t) value) + 500000) >> 6) * 67) >> 20), 3072)
+#define PRECISION_CONVERTER_TRIG_PARAM_F2200(value) CLAMP(((((((uint32_t) value) + 500000) >> 6) * 67) >> 20), 3072)
 
-#define QUANTS_COUNT_PER_SYMBOL 80
+#define INVERSE_TRIG_PARAM_SCALER ((uint32_t)750)
+#define PRECISION_CONVERTER_INVERSE_TRIG_PARAM(value) CLAMP(((((((uint32_t) value) + 500000) >> 6) * 67) >> 20), 3072)
 
-#define RECIPROCAL_ANGULAR_FREQUENCY_F1200 127324
-#define RECIPROCAL_ANGULAR_FREQUENCY_F2200 69449
+#define QUANTS_COUNT_PER_SYMBOL ((uint32_t)80)
 
-#define LEADING_WARMUP_QUANTS_COUNT 960
+#define RECIPROCAL_ANGULAR_FREQUENCY_F1200 ((uint32_t)127324)
+#define RECIPROCAL_ANGULAR_FREQUENCY_F2200 ((uint32_t)69449)
+
+#define LEADING_WARMUP_QUANTS_COUNT ((uint32_t)960)
 
 extern const uint16_t amplitudeFromTable[];
 extern const uint32_t scaledSineValueFromTable[];
